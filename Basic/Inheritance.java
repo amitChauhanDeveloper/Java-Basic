4b3a45039689c8b090a3b214c3cc487771fbf82d
shreya.shah <shreya.s@zyapaar.com>
2023-02-09 18:27:20 +0530

    Changes

--- Basic/Inheritance.java
+++ Basic/Inheritance.java
@@ -1,9 +1,25 @@
 package Basic;
 
-public class Inheritance {
+/* class Employee{  
+    float salary=40000;  
+   }  
+   class Programmer extends Employee{  
+    int bonus=10000;  
+    public static void main(String args[]){  
+      Programmer p=new Programmer();  
+      System.out.println("Programmer salary is:"+p.salary);  
+      System.out.println("Bonus of Programmer is:"+p.bonus);  
+   }  
+   }   */
 
-    public static void main(String[] args) {
-        
-    }
-    
-}
+   class Inheritance{  
+    float salary=40000;  
+   }  
+   class SubClass extends Inheritance{  
+    int bonus=10000;  
+    public static void main(String args[]){  
+        SubClass p=new SubClass();  
+      System.out.println("Programmer salary is:"+p.salary);  
+      System.out.println("Bonus of Programmer is:"+p.bonus);  
+   }  
+   }  
