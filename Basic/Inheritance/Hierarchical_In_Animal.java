cae2942f508df71f3f0ab4875a0a986d4d097f91
shreya.shah <shreya.s@zyapaar.com>
2023-02-13 10:13:54 +0530

    Changes

--- Basic/Inheritance/Hierarchical_In_Animal.java
+++ Basic/Inheritance/Hierarchical_In_Animal.java
@@ -1,5 +1,32 @@
 package Basic.Inheritance;
 
-public class Hierarchical_In_Animal {
-    
+class Animal {
+
+  void eat() {
+    System.out.println("eating...");
+  }
+}
+
+class Dog extends Animal {
+
+  void bark() {
+    System.out.println("barking...");
+  }
+}
+
+class Cat extends Animal {
+
+  void meow() {
+    System.out.println("meowing...");
+  }
+}
+
+class Hierarchical_In_Animal {
+
+  public static void main(String args[]) {
+    Cat c = new Cat();
+    c.meow();
+    c.eat();
+    //c.bark();//C.T.Error
+  }
 }
