035a7cda5b06067e147fbbcd8826a304c69d6de5
shreya.shah <shreya.s@zyapaar.com>
2023-02-14 12:49:51 +0530

    Add File

--- Basic/Final_Keyword/Final_Parameter.java
+++ Basic/Final_Keyword/Final_Parameter.java
@@ -1,12 +1,12 @@
 package Basic.Final_Keyword;
 
-class Bike11{  
+class Bike{  
     int cube(final int n){  
      n=n+2;//can't be changed as n is final  
      n*n*n;  
     }  
     public static void main(String args[]){  
-      Bike11 b=new Bike11();  
+      Bike b=new Bike();  
       b.cube(5);  
    }  
   }
