a9ea66af1bf999c3b00fac13c0570ebf388381ed
shreya.shah <shreya.s@zyapaar.com>
2023-02-15 11:32:24 +0530

    Add File

--- Basic/Polymorphism/Example_with_data_member.java
+++ Basic/Polymorphism/Example_with_data_member.java
@@ -1,5 +1,23 @@
 package Basic.Polymorphism;
 
-public class Example_with_data_member {
-    
+/* program not run ( Exception in thread "main" java.lang.NoSuchFieldError: speedlimit
+        at Basic.Polymorphism.Example_with_data_member.main(Example_with_data_member.java:18) ) */
+        
+//Runtime polymorphism can't be achieved by data members.
+class Bike {
+
+  int speedlimit = 90;
+}
+
+class Honda3 extends Bike {
+
+  int speedlimit = 150;
+}
+
+class Example_with_data_member {
+
+  public static void main(String args[]) {
+    Bike obj = new Honda3();
+    System.out.println(obj.speedlimit); //90
+  }
 }
